3074b2c77220bce8593e2757f6e377725d482cb9
Fen <fendev@protonmail.com>
2018-05-16 16:33:40 +1000

    corrected typo for exercise

--- 01_small_problems/07_easy_seven/08_multiply_lists.rb
+++ 01_small_problems/07_easy_seven/08_multiply_lists.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 =begin
   Write a method that takes two Array arguments in which each Array contains a list of numbers, and returns a new Array that contains the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments contain the same number of elements.
 
@@ -13,5 +14,10 @@ def multiply_list(ary1, ary2)
   new_ary
 end
 
-puts multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+# Option B:
+def multiply_list(ary1, ary2)
+  ary1.zip(ary2).map { |a1, a2| a1 * a2 }
+end
 
+
+puts multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
