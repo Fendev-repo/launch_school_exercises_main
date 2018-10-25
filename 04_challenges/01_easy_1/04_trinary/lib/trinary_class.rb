2b86f075a4093d7fa7fa8400bb394a8ca1f1119c
FenDev <fendev@protonmail.com>
2018-10-25 12:51:14 +1100

    fixed some typos with rubocop

--- 04_challenges/01_easy_1/04_trinary/lib/trinary_class.rb
+++ 04_challenges/01_easy_1/04_trinary/lib/trinary_class.rb
@@ -1,6 +1,6 @@
 # A class that will convert a trinary number, represented as a string
 class Trinary
-  TRINARY = 3 
+  TRINARY = 3
 
   def initialize(trinary_str)
     @trinary_str = trinary_str
@@ -13,10 +13,11 @@ class Trinary
 
   def to_decimal
     return 0 if valid_trinary?
+
     decimals = []
 
     @trinary_int_array.each_with_index do |number, exponent|
-       decimals << (number * (TRINARY ** exponent))
+      decimals << (number * (TRINARY**exponent))
     end
     decimals.sum
   end
--- 04_challenges/01_easy_1/04_trinary/test/test_trinary_class.rb
+++ 04_challenges/01_easy_1/04_trinary/test/test_trinary_class.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
 require_relative '../lib/trinary_class'
 
